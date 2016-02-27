90ab223a3be82e13f65808f93ef35c6498b18c72
Elliott Beach <elliott2.71828@gmail.com>
2016-02-27 16:05:02 -0600

    renamed and modified

--- src/simple_search/genetic_search.clj
+++ src/simple_search/genetic_search.clj
@@ -1,4 +1,4 @@
-(ns genetic-search
+(ns simple-search.genetic-search
   (:require [simple-search.core :as core])
    (:use simple-search.knapsack-examples.knapPI_11_20_1000
         simple-search.knapsack-examples.knapPI_13_20_1000
@@ -11,21 +11,41 @@
   [answer]
   (core/add-score core/penalized-score answer))
 
+(defn wild-type
+  [instance]
+  "generate lazy seq of random individuals"
+  (repeat (add-score (core/random-answer instance))))
+
 (defn first-generation
   "generate num random answers for instance"
   [instance num]
-  (for [i (range num) ] (add-score (core/random-answer instance))))
+  (take num (set (wild-type instance))))
 
 (defn mutate-pop
   [population]
-  (map  #(add-score (core/mutate-answer %))  population))
+  (map  #(add-score (core/mutate-answer %)) population))
 
 (defn next-generation
+  "create a new generation from a population by mutating it, and taking the best
+  n out of old and young combined, where n is population size"
   [population]
   (->> population
       (concat (mutate-pop population))
       (sort-by #(-> % :score - ))
       (take (count population))))
+
+(defn next-gen-no-duplicates
+  "like next-generation, but remove duplicates in the population. Fill in with wilds if we run out of room
+  THOUGHT: we should use a set instead of a list for the population"
+  [population]
+  (let [instance (:instance (first population))
+        fill-in-wilds #(lazy-cat % (wild-type instance))]
+    (->> population
+        (concat (mutate-pop population))
+        (sort-by #(-> % :score - ))
+        distinct
+        fill-in-wilds
+        (take (count population)))))
 
 (defn mutate-search
   "generate an answer through randomly mutating a population"
@@ -36,6 +56,5 @@
         bestest (apply max-key :score final-pop)]
     bestest))
 
-(mutate-search knapPI_11_20_1000_4 200)
-
+;;(mutate-search knapPI_11_20_1000_4 200)
 ;; (next-generation (first-generation knapPI_13_20_1000_20 10 ))
