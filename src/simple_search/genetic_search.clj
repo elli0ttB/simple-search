43e19cddc6aa0de1e8ebb6b474073116eff04cae
Elliott Beach <beach144@morris.umn.edu>
2016-02-27 19:03:17 -0600

    Added uniform crossover, figured out distinct

--- src/simple_search/genetic_search.clj
+++ src/simple_search/genetic_search.clj
@@ -7,6 +7,8 @@
         simple-search.knapsack-examples.knapPI_13_200_1000
         simple-search.knapsack-examples.knapPI_16_200_1000))
 
+(defmacro dbg[x] `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
+
 (defn add-score
   [answer]
   (core/add-score core/penalized-score answer))
@@ -14,47 +16,80 @@
 (defn wild-type
   [instance]
   "generate lazy seq of random individuals"
-  (repeat (add-score (core/random-answer instance))))
+  (repeatedly #(add-score (core/random-answer instance))))
+
+
+(take 10 (distinct (wild-type knapPI_11_20_1000_7)))
+
+
+(defn get-distinct
+  "gets n distinct items from lazy seq of items"
+  [num lazyseq]
+
+  let [get-distinct #(distinct (take % lazyseq))
+       add-more (fn [items]
+                  (let [diff (- num (count items))
+                        new-items (concat items (get-distinct diff))]
+                    (if (< (count new-items) num)
+                      (recur new-items)
+                      new-items)))]
+         (add-more '()))
 
 (defn first-generation
   "generate num random answers for instance"
   [instance num]
-  (take num (set (wild-type instance))))
+  (take num (wild-type instance)))
+  ;(get-distinct num (wild-type instance)))
+
+
+(first-generation knapPI_11_20_1000_1 10)
+
 
 (defn mutate-pop
+  "generate children of population using mutation from core.clj"
   [population]
   (map  #(add-score (core/mutate-answer %)) population))
 
-(defn next-generation
+(defn modify-generation
   "create a new generation from a population by mutating it, and taking the best
   n out of old and young combined, where n is population size"
-  [population]
+  [population mutator]
   (->> population
-      (concat (mutate-pop population))
+      (concat (mutator population))
       (sort-by #(-> % :score - ))
+      distinct
       (take (count population))))
 
-(defn next-gen-no-duplicates
-  "like next-generation, but remove duplicates in the population. Fill in with wilds if we run out of room
-  THOUGHT: we should use a set instead of a list for the population"
-  [population]
-  (let [instance (:instance (first population))
-        fill-in-wilds #(lazy-cat % (wild-type instance))]
-    (->> population
-        (concat (mutate-pop population))
-        (sort-by #(-> % :score - ))
-        distinct
-        fill-in-wilds
-        (take (count population)))))
 
 (defn mutate-search
   "generate an answer through randomly mutating a population"
-  [instance evals]
+  [instance mutator evals]
   (let [start (first-generation instance 100)
-        generations (iterate next-generation start)
+        modify #(modify-generation % mutator)
+        generations (iterate modify start)
         final-pop (nth generations evals)
         bestest (apply max-key :score final-pop)]
     bestest))
 
-;;(mutate-search knapPI_11_20_1000_4 200)
-;; (next-generation (first-generation knapPI_13_20_1000_20 10 ))
+ (mutate-search knapPI_11_20_1000_4 mutate-pop 100)
+
+ (defn tournament
+  [population num-competitors]
+  (apply max-key :score (take num-competitors (shuffle population))))
+
+(defn crossover-mutator
+  "use tournament selection to generate children"
+  [population crossing]
+  (let [gen-winner #(tournament population 10)
+        gen-child #(crossing (gen-winner) (gen-winner))]
+    (repeat (count population) (gen-child))))
+
+
+(defn uniform-crossover
+  "Take two answers and create a third one using uniform crossover"
+  [answer-1 answer-2]
+  (let [answer-vector (map vec (:choices answer-1) (:choices answer-2))
+        new-choices (map rand-nth answer-vector)]
+    (add-score (core/make-answer (:instance answer-1) new-choices))))
+
+
