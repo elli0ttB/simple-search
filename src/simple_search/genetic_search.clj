c0e90b5352ca9aa3b8ae45a197f08ba5f38518a2
Elliott Beach <elliott2.71828@gmail.com>
2016-02-28 03:23:41 -0600

    refactored && leveled up

--- src/simple_search/genetic_search.clj
+++ src/simple_search/genetic_search.clj
@@ -13,30 +13,25 @@
   [answer]
   (core/add-score core/penalized-score answer))
 
+(defn make-answer
+  "wrapper around core/make-answer"
+  [instance choices]
+  (add-score (core/make-answer instance choices)))
 
 (defn wild-type
   [instance]
   "generate lazy seq of random individuals"
   (repeatedly #(add-score (core/random-answer instance))))
 
-
 (defn best
   "take a population and return answer with highest score"
   [population]
   (apply max-key :score population))
 
-
 (defn first-generation
   "generate num random answers for instance"
   [instance num]
   (take num (wild-type instance)))
-
-
-(defn mutate-pop
-  "generate children of population using mutation from core.clj"
-  [population]
-  (map  #(add-score (core/mutate-answer %)) population))
-
 
 (defn modify-generation
   "create a new generation from a population by mutating it, and taking the best
@@ -48,61 +43,65 @@
       dedupe
       (take (count population))))
 
-
 (defn mutate-search
   "generate an answer through randomly mutating a population"
-  [instance mutator evals]
-  (let [start (first-generation instance 100)
+  [instance mutator evals pop-size]
+  (let [start (first-generation instance pop-size)
         modify #(modify-generation % mutator)
-        generatins (iterate modify start)
-        final-pop(nth generations evals)]
-    (best final-pp)))
+        generations (iterate modify start)
+        final-pop (nth generations evals)]
+    (best final-pop)))
 
-
- (defn tournament
+(defn tournament
   [population num-competitors]
   (best (take num-competitors (shuffle population))))
 
+(defn bits-to-ans
+  "take a function that operates on bits returns an equivalent function that operates on answers"
+  [bit-mutator]
+  (fn [& answers]
+    (let [choices (map :choices answers)
+          instance (:instance (first answers)) ]
+    (make-answer instance (apply bit-mutator choices)))))
 
 (defn crossover-mutator
-  "use tournament selection to generate children"
+  "use tournament selection to generate children with a given algorithm, given a genetic crossing"
   [population crossing]
   (let [gen-winner #(tournament population 10)
-        gen-child #(crossing (gen-winner) (gen-winner))]
+        gen-child #( (bits-to-ans crossing) (gen-winner) (gen-winner) )]
     (repeatedly (count population) gen-child )))
 
+(defn get-randoms
+  "return a sorted list of random numbers between 0 and max"
+  [length maximum]
+  (sort (repeatedly length #(rand-int maximum))))
 
 (defn two-point-crossover
-  [answer-1 answer-2]
-  (let [choices-1 (:choices answer-1)
-        choices-2 (:choices answer-2)
-        size (count choices-1)
-        [first-split last-split] (sort (repeatedly 2 #(rand-int size)))
+  "take two bit lists and return a new answer with two cuts"
+  [choices-1 choices-2]
+  (let [[first-split last-split] (get-randoms 2 (count choices-1))]
+      (concat
+          (take first-split choices-1)
+          (->> choices-2
+                (drop first-split)
+                (take (- last-split first-split)))
+          (drop last-split choices-1))))
 
-        new-choices (concat
-                      (take first-split choices-1)
+(defn uniform-crossover [choices-1 choices-2]
+  (map (comp rand-nth vector) choices-1 choices-2))
 
-                      (->> choices-2
-                           (drop first-split)
-                           (take (- last-split first-split)))
+(defn mutate-pop
+  "generate three-armed children of population using mutation from core.clj"
+  [population]
+  (map  #(add-score (core/mutate-answer %)) population))
 
-                      (drop last-split choices-1))]
+;;; testing stuff woohoo
 
-    (add-score (core/make-answer (:instance answer-1) new-choices))))
-
-
-(apply two-point-crossover (take 2 (wild-type knapPI_11_20_1000_1)))
-
-(defn uniform-crossover
-  "Take two answers and create a third one using uniform crossover"
-  [answer-1 answer-2]
-  (let [answer-vector (map vector (:choices answer-1) (:choices answer-2))
-        new-choices (map rand-nth answer-vector)]
-    (add-score (core/make-answer (:instance answer-1) new-choices))))
+(apply (bits-to-ans uniform-crossover) (take 2 (wild-type knapPI_11_20_1000_1)))
 
 (let [uniform-crossover (fn [pop] (crossover-mutator pop uniform-crossover))
       two-point-crossover (fn [pop] (crossover-mutator pop two-point-crossover))]
-(mutate-search knapPI_16_20_1000_63 two-point-crossover 200))
+(mutate-search knapPI_16_20_1000_63 two-point-crossover 100 30))
 
 
 
